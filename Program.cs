b41852bd95b0977badd99ba1e774272925d8bd9c
Лидия Обыденнова <lida.obydennova@yandex.ru>
2022-11-23 23:44:11 +0300

    Поиск самого большого числа

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] numbers = { 1, 6, 8, -7, 5, 21, 100, 1064, 1124, -348 };
-int temp;
-for (int i = 0; i < numbers.Length; i++)
+int[] numbers = { 1, 3, 4, 5, 10, 7, 100, 12, 10, 20 };
+int temporary = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
 {
-    for (int j = i + 1; j < numbers.Length; j++)
+    if (temporary <= numbers[i])
     {
-        if (numbers[i] > numbers[j])
-        {
-            temp = numbers[i];
-            numbers[i] = numbers[j];
-            numbers[j] = temp;
-        }
+        temporary = numbers[i];
     }
 }
-Console.WriteLine("Максимальное число массива: " + numbers[numbers.Length - 1]);
+Console.WriteLine("Самое большое число в массиве: " + temporary);
